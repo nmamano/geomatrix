f31e32bd2fd750a663eeaaebeb496d237d2b731b
nmamano <tehotserver@gmail.com>
2013-06-04 14:54:26 +0200

    Initial commit.

---
+++ src/geomatrix/Geomatrix.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geomatrix;
+
+/**
+ *
+ * @author Nil
+ */
+public class Geomatrix {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
